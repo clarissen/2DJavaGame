ce12e72c30109d553bee616f3aa1dc87acbc6066
clarissen <nclarisse26@gmail.com>
2025-03-25 20:25:31 -0500

    added a res folder inside entity that contains sprites for player. modified player class

--- entity/Entity.java
+++ entity/Entity.java
@@ -1,9 +1,14 @@
 package entity;
+
+import java.awt.image.BufferedImage;
+
 // stores variables used in all player and non-player classes
 public class Entity{
 
     public int x, y;
     public int speed;
 
-    
+    // use BufferedImage to store image files
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
 }
--- entity/Player.java
+++ entity/Player.java
@@ -2,6 +2,10 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
@@ -17,27 +21,52 @@ public class Player extends Entity{
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues(){
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
 
+    public void getPlayerImage(){
+        try {
+            
+            // up1 = ImageIO.read(getClass().getResourceAsStream(path+"boy_up_1.png"));
+            String path = "/entity/res/player/walking_sprites/";
+            up1 = ImageIO.read(getClass().getResource(path+"boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResource(path+"boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResource(path+"boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResource(path+"boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResource(path+"boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResource(path+"boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResource(path+"boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResource(path+"boy_right_2.png"));
+
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void update(){
 
         if(keyH.upPressed == true){
+            direction = "up";
             y -= speed;
         }
         else if(keyH.downPressed == true){
+            direction = "down";
             y += speed;
         }
         else if(keyH.leftPressed == true){
+            direction = "left";
             x -= speed;
         }
         else if(keyH.rightPressed == true){
+            direction = "right";
             x += speed;
         }
 
@@ -45,9 +74,23 @@ public class Player extends Entity{
 
     public void draw(Graphics2D g2){
 
-        g2.setColor(Color.white);
+        BufferedImage image = null;
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        switch(direction){
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;  
+            case "left":
+                image = left1;
+                break;  
+            case "right":
+                image = right1;
+                break;    
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 
     }
 }
